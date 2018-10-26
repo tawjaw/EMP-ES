d855b18773021ef17d3ade297562819730c3e247
Tawfiq Jawhar <tawjaw@gmail.com>
2018-10-26 03:00:48 -0400

    modified files

--- generatetest.sh
+++ generatetest.sh
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 mkdir -p CCscripts/BT/4x4
 cd CCscripts/BT/4x4
 
@@ -13,4 +14,14 @@ module load python/3.7
 	
 python CCscripts/BT/BT.py '/project/6000253/tjawhar/EMP-ES/ESData/S/4x4/"$i"/' 4 " >> "$i".sh
 done
+=======
+
+mkdir -p scripts/BT/3x3/2
+i=1
+for FILE in ESData/S/3x3/2/*.csv; do
+	echo "python CCscripts/BT/BT.py '/project/6000253/tjawhar/EMP-ES/ESData/S/3x3/2/' '"$FILE "' 3 'EMP-ES/ESResults/BT/3x3/2/'" > scripts/BT/3x3/2/$i.sh
+	i=$(($i+1))
+done
+	
+>>>>>>> Stashed changes
 
